076da5cf2274536b5edda61cd4da02e4c931a002
pcaparros <pcaparros@openit.com>
2022-04-15 21:26:15 +0800

    Save inital login and register

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 
 import 'firebase_options.dart';
@@ -26,7 +27,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Register'),
+        title: const Text('Home'),
       ),
       body: FutureBuilder(
         future: Firebase.initializeApp(
@@ -41,14 +42,44 @@ class HomePage extends StatelessWidget {
                 print('You are a verified user');
               } else {
                 print('You need to verify your email first');
+                // Navigator.of(context).push(
+                //   MaterialPageRoute(
+                //     builder: (context) => const VerifyEmailView(),
+                //   ),
+                // );
+                return const VerifyEmailView();
               }
-              print(FirebaseAuth.instance.currentUser);
               return const Text('Done');
             default:
               return const Text('Loading...');
           }
         },
       ),
+    );
+  }
+}
+
+class VerifyEmailView extends StatefulWidget {
+  const VerifyEmailView({Key? key}) : super(key: key);
+
+  @override
+  State<VerifyEmailView> createState() => _VerifyEmailViewState();
+}
+
+class _VerifyEmailViewState extends State<VerifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text('Please verify your email address'),
+        TextButton(
+            onPressed: () async {
+              final user = FirebaseAuth.instance.currentUser;
+              print(user);
+              await user?.sendEmailVerification();
+            },
+            child: const Text('Senf email verification'))
+      ],
     );
   }
 }
